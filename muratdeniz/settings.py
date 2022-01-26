096ad814286a7356802a8ca2b553dc506c93b5ef
darkk_bluee <yucelakpolat@gmail.com>
2022-01-27 01:15:48 +0300

    first commit

--- muratdeniz/settings.py
+++ muratdeniz/settings.py
@@ -130,10 +130,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'static') # yeni sa
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # yeni sa
 #STATICFILES_DIRS = [str(BASE_DIR.joinpath('static')),] # yeni satır
 MEDIA_URL ='/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media') # yeni sa
+MEDIA_ROOT = os.path.join(BASE_DIR,'media/') # yeni sa
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
