e28b1bd669c59d72530157fc8fd283d4c1c4b5f1
darkk_bluee <yucelakpolat@gmail.com>
2022-01-27 10:01:07 +0300

    first commit

--- muratdeniz/settings.py
+++ muratdeniz/settings.py
@@ -135,7 +135,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # yeni sa
 #STATICFILES_DIRS = [str(BASE_DIR.joinpath('static')),] # yeni satır
-MEDIA_URL ='/static/'
+MEDIA_URL ='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media') # yeni sa
 
 # Default primary key field type
