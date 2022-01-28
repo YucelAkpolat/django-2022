5c93768fa97137636c47857595df9eb609ec919b
darkk_bluee <yucelakpolat@gmail.com>
2022-01-28 05:55:58 +0300

    first commit

--- muratdeniz/settings.py
+++ muratdeniz/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_URL  ='/media/'
+MEDIA_URL  ='media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # yeni sa
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
