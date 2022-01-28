1c889dea5709d4198c0d32752321de011d19fa3b
darkk_bluee <yucelakpolat@gmail.com>
2022-01-28 05:31:14 +0300

    first commit

--- muratdeniz/settings.py
+++ muratdeniz/settings.py
@@ -136,7 +136,7 @@ STATIC_URL = '/static/'
 MEDIA_URL  ='/media/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # yeni sa
-MEDIA_ROOT = [os.path.join(BASE_DIR, 'media')]
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 
